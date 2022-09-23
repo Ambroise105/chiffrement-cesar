7c6462d31502863a3bdc5e9f6692efb6e446c4ea
Alex <alex.ardiot@gmail.com>
2022-09-23 10:39:52 +0200

    Fonction dechiffrement

--- cesar.py
+++ cesar.py
@@ -9,7 +9,7 @@ def chiffrement(texte, offset):
     valeur = ""
 
     for i in texte:
-        
+
         valeur = ord(i)+offset
 
         if valeur > 122:
@@ -21,9 +21,38 @@ def chiffrement(texte, offset):
     return message_chiffre
 
 
+def dechiffrement(texte, offset):
+    """Fonction pour déchiffrer le message de l'utilisateur
+    param1: texte(str) - Texte à déchiffrer
+    param2: offset(int) - Clé de déchiffrement utilisé
+    return: message_dechiffre(str) - Message déchiffré
+    """
+
+    message_dechiffre = ""
+    valeur = ""
+
+    for i in texte:
+        
+        valeur = ord(i)-offset
+
+        if valeur < 97:
+            message_dechiffre += chr((valeur)+26)
+
+        else:
+            message_dechiffre += chr(valeur)
+
+    return message_dechiffre
+
+
 
 clair = input("Quel est votre message : ")
-cle = int(input("Quel est la clé de chiffrement : "))
+cle_chiffrement = int(input("Quel est la clé de chiffrement : "))
 
 
-print(chiffrement(clair, cle))
+print(chiffrement(clair, cle_chiffrement))
+
+
+chiffre = input("Quel est votre message chiffré : ")
+cle_dechiffrement = int(input("Quel est la clé de déchiffrement : "))
+
+print(dechiffrement(chiffre, cle_dechiffrement))
